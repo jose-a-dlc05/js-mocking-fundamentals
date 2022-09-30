257619722ccdf9741b0d328a4c758cf7b6acc03d
Jose De La Cruz <jose.delcruz0490@icloud.com>
2022-09-29 20:17:58 -0400

    Restored the original implementation with jest.spyOn

--- src/__tests__/mock-fn.js
+++ src/__tests__/mock-fn.js
@@ -18,8 +18,8 @@ const thumbWar = require('../thumb-war');
 const utils = require('../utils');
 
 test('returns winner', () => {
-	const originalGetWinner = utils.getWinner;
-	utils.getWinner = jest.fn((p1, p2) => p1);
+	jest.spyOn(utils, 'getWinner');
+	utils.getWinner.mockImplementation((p1, p2) => p1);
 
 	const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler');
 	expect(winner).toBe('Kent C. Dodds');
@@ -42,7 +42,7 @@ test('returns winner', () => {
 	]);
 
 	// cleanup
-	utils.getWinner = originalGetWinner;
+	utils.getWinner.mockRestore();
 });
 
 /**
